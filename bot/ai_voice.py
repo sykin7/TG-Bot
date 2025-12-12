2d176188f45df10f71037f2e72d292bef8c54607
sykin7 <sykin715@gmail.com>
2025-12-12 13:55:57 +0800

    Delete bot/ai_voice.py

--- bot/ai_voice.py
+++
@@ -1,7 +0,0 @@
-import openai, os
-openai.api_key = os.environ.get("OPENAI_API_KEY")
-
-async def transcribe_voice(path: str):
-    audio_file = open(path, "rb")
-    transcript = openai.Audio.transcriptions.create(file=audio_file, model="whisper-1")
-    return transcript.text
