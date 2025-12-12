5919e4ac562134a959952b99b5bdba4a2da3a583
sykin7 <sykin715@gmail.com>
2025-12-12 13:42:35 +0800

    Create logger.py

---
+++ bot/utils/logger.py
@@ -0,0 +1,7 @@
+import logging
+logger = logging.getLogger("TGBot")
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
