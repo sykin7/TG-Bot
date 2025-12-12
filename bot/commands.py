25b837896169da4494f9d46dab106e43d3026111
sykin7 <sykin715@gmail.com>
2025-12-12 13:56:05 +0800

    Delete bot/commands.py

--- bot/commands.py
+++
@@ -1,5 +0,0 @@
-from telegram import Update
-from telegram.ext import CallbackContext
-
-async def start_command(update: Update, context: CallbackContext.DEFAULT_TYPE):
-    await update.message.reply_text("TG Bot 已启动，功能齐全！")
