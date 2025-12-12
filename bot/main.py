e7de2aeed55615c1134011eb2ac71121b3d8cf10
sykin7 <sykin715@gmail.com>
2025-12-12 13:46:31 +0800

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,15 +1,14 @@
 import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, CallbackContext
-from bot.commands import start_command
-from bot.handlers import handle_message, handle_voice, handle_file
-from bot.scheduler import start_scheduler
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from bot.commands.start import start_command
+from bot.handlers.message import handle_message
+from bot.handlers.voice import handle_voice
+from bot.handlers.file import handle_file
+from bot.scheduler.tasks import start_scheduler
 from dotenv import load_dotenv
 
 load_dotenv('../config/config.env')
-
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
 app.add_handler(CommandHandler("start", start_command))
@@ -18,5 +17,4 @@ app.add_handler(MessageHandler(filters.VOICE, handle_voice))
 app.add_handler(MessageHandler(filters.Document.ALL, handle_file))
 
 start_scheduler(app)
-
 app.run_polling()
